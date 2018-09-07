676ce37218c35f1c1e0e00ea4c6fd664a897b792
Evan Bolyen <ebolyen@gmail.com>
2018-09-07 14:35:00 -0700

    Fix git commit order

--- bin/copy-env.sh
+++ bin/copy-env.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-git clone ./updated-built-iab ./final-built-iab
-
 # Configure git
 git config --global user.name iabb8
 git config --global user.email "gregcaporaso+iabb8@gmail.com"
 
-cp iab-source/Dockerfile.template final-built-iab/Dockerfile
-cp iab-source/environment.yml final-built-iab/
+cp iab-source/Dockerfile.template updated-built-iab/Dockerfile
+cp iab-source/environment.yml updated-built-iab/
 
-cd final-built-iab
+cd updated-built-iab
 git add .
 git diff-index --quiet HEAD || git commit -m "Updated rendered notebooks."
+
+cd ..
+git clone ./updated-built-iab ./final-built-iab
