5699c3f2c27ab9fcdfb1a9a0b32021d6b643484e
Evan Bolyen <ebolyen@gmail.com>
2018-09-07 12:43:39 -0700

    WIP: should work

--- bin/copy-env.sh
+++ bin/copy-env.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+git clone ./updated-built-iab ./final-built-iab
+
 # Configure git
 git config --global user.name iabb8
 git config --global user.email "gregcaporaso+iabb8@gmail.com"
 
-cp iab-source/Dockerfile.template updated-built-iab/Dockerfile
-cp iab-source/environment.yml updated-built-iab/
+cp iab-source/Dockerfile.template final-built-iab/Dockerfile
+cp iab-source/environment.yml final-built-iab/
 
-cd updated-built-iab
+cd final-built-iab
 git add .
 git diff-index --quiet HEAD || git commit -m "Updated rendered notebooks."
